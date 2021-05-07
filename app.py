37f352cfefe41b9bf1c3b15715d0ef79587d2ba9
yuehhsin <75563062+yuehhsin@users.noreply.github.com>
2021-05-07 14:43:55 +0800

    0.3.4
    [前端]
    +增加一些互動細節

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 mydb=mysql.connector.connect(
 	host="localhost",
 	user="root",
-	password="KElly_7991",
+	password="KElly7991",
 	database="gov_data",
 	charset="utf8",
 )
@@ -172,4 +172,4 @@ def handle_500(e):
 	}
 	return jsonify(error),500
 
-app.run(host="0.0.0.0",port=3000)
+app.run(host="0.0.0.0",port=3000,debug=True)
